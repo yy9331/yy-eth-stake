c656f4ef0a8e8923cb8fbb170dad124d9f1d4d26
1 <yuyi.gz@163.com>
2025-07-15 16:09:09 +0800

    init project

--- stake-contract/hardhat.config.js
+++ stake-contract/hardhat.config.js
@@ -5,7 +5,7 @@ require("@openzeppelin/hardhat-upgrades");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.22",
     settings: {
       optimizer: {
         enabled: false,
